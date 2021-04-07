16eef5a3af3d34266e00108570367e5489895175
SuperCrabLover <molozhavenko.aa@phystech.edu>
2021-04-08 00:39:18 +0300

    fix rotate calculating

--- Runge.h
+++ Runge.h
@@ -1,4 +1,4 @@
 static const double one_sixth = 1.0 / 6.0;
-void Runge_Kutta_2nd_Order( double (*f)(double, double, double), double x0,
-                        double y[], double c, double h, int number_of_steps );
+void Runge_Kutta_2nd_Order( double (*f)(double, double, double, double, double), double x0,
+                        double y[], double& c, double h, int number_of_steps );
 
